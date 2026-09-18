98e6eff6a3a3d2e245ac09253f83a747d8d0b7bf
doniyornazarov520-lang <doniyornazarov520@gmail.com>
2026-09-18 22:27:01 +0500

    Delete App.jsx

--- App.jsx
+++
@@ -1,7 +0,0 @@
-import ProvodkaSimulator from './ProvodkaSimulator';
-
-// ... App komponentingiz ichida content qismining pastki qismiga qo'shasiz:
-
-<div style={styles.card}>
-  <ProvodkaSimulator currentLessonId={selectedLesson.id} />
-</div>
