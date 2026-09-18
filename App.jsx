33b6299c89aadb7801366a24e92e37d242a568f9
doniyornazarov520-lang <doniyornazarov520@gmail.com>
2026-09-18 21:33:03 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,7 @@
+import ProvodkaSimulator from './ProvodkaSimulator';
+
+// ... App komponentingiz ichida content qismining pastki qismiga qo'shasiz:
+
+<div style={styles.card}>
+  <ProvodkaSimulator currentLessonId={selectedLesson.id} />
+</div>
